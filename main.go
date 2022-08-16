895c151348a207f07e42ab2de828cf414f46bdcd
TunGuLaMianYG <66915631+TunGuLaMianYG@users.noreply.github.com>
2022-08-16 08:08:51 +0800

    first commit

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
-<!--
+/*
  * @Author: TunGuLaMianYG 66915631+TunGuLaMianYG@users.noreply.github.com
- * @Date: 2022-08-16 07:56:22
+ * @Date: 2022-08-16 07:59:32
  * @LastEditors: TunGuLaMianYG 66915631+TunGuLaMianYG@users.noreply.github.com
- * @LastEditTime: 2022-08-16 07:57:45
- * @FilePath: \goblog\README.md
+ * @LastEditTime: 2022-08-16 08:05:02
+ * @FilePath: \goblog\main.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
-# goblog
-gin+vue全栈博客
-
-test
+ */
+package main
