a86e17992af476a4dc295f9c8c02699e0ee0472c
TunGuLaMianYG <857588030@qq.com>
2022-08-16 15:22:16 +0800

    删除:   提示信息

--- main.go
+++ main.go
@@ -4,7 +4,6 @@
  * @LastEditors: TunGuLaMianYG 66915631+TunGuLaMianYG@users.noreply.github.com
  * @LastEditTime: 2022-08-16 08:05:02
  * @FilePath: \goblog\main.go
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 package main
 
