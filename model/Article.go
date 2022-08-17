05ee9f0786bb28051769fbda6ef109a573249d72
TunGuLaMianYG <66915631+TunGuLaMianYG@users.noreply.github.com>
2022-08-17 22:15:39 +0800

    Update Article.go

--- model/Article.go
+++ model/Article.go
@@ -1,1 +1,9 @@
+/*
+ * @Author: TunGuLaMianYG 66915631+TunGuLaMianYG@users.noreply.github.com
+ * @Date: 2022-08-17 21:38:56
+ * @LastEditors: TunGuLaMianYG 66915631+TunGuLaMianYG@users.noreply.github.com
+ * @LastEditTime: 2022-08-17 22:14:28
+ * @FilePath: \goblog\model\Article.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 package model
