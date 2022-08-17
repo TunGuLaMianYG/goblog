cf5f263318f54007895aba183fd3df1f9f38cc4e
TunGuLaMianYG <66915631+TunGuLaMianYG@users.noreply.github.com>
2022-08-17 08:17:35 +0800

    modified:   utils/setting.go

--- utils/setting.go
+++ utils/setting.go
@@ -2,7 +2,7 @@
  * @Author: TunGuLaMianYG 66915631+TunGuLaMianYG@users.noreply.github.com
  * @Date: 2022-08-17 08:08:05
  * @LastEditors: TunGuLaMianYG 66915631+TunGuLaMianYG@users.noreply.github.com
- * @LastEditTime: 2022-08-17 08:16:47
+ * @LastEditTime: 2022-08-17 08:17:13
  * @FilePath: \goblog\utils\setting.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
