ae3a24b8ce0fc31c8c3303594bfb514f612ee6b0
TunGuLaMianYG <66915631+TunGuLaMianYG@users.noreply.github.com>
2022-08-18 08:16:02 +0800

    Update mysql.go

--- model/mysql.go
+++ model/mysql.go
@@ -2,7 +2,7 @@
  * @Author: TunGuLaMianYG 66915631+TunGuLaMianYG@users.noreply.github.com
  * @Date: 2022-08-17 21:39:52
  * @LastEditors: TunGuLaMianYG 66915631+TunGuLaMianYG@users.noreply.github.com
- * @LastEditTime: 2022-08-18 08:15:19
+ * @LastEditTime: 2022-08-18 08:15:39
  * @FilePath: \goblog\model\mysql.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
