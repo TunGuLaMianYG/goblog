1be5595c4190060611c68f1b5b05803aa3a4aca1
TunGuLaMianYG <66915631+TunGuLaMianYG@users.noreply.github.com>
2022-08-23 08:12:33 +0800

    modified:   module/User.go

--- module/User.go
+++ module/User.go
@@ -2,7 +2,7 @@
  * @Author: TunGuLaMianYG 66915631+TunGuLaMianYG@users.noreply.github.com
  * @Date: 2022-08-17 21:38:40
  * @LastEditors: TunGuLaMianYG 66915631+TunGuLaMianYG@users.noreply.github.com
- * @LastEditTime: 2022-08-22 22:21:15
+ * @LastEditTime: 2022-08-23 08:12:18
  * @FilePath: \goblog\model\User.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
